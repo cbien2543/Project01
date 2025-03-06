350b54bb5a1b2663929d98afc1d14b0098099d67
cbien2543 <chrisbienvenu03@gmail.com>
2025-03-05 18:15:32 -0600

    Delay Effect

---
+++ AirDelay/Source/DelayEffectProcessor.h
@@ -0,0 +1,48 @@
+/*
+  ==============================================================================
+
+    DelayEffectProcessor.h
+    Created: 5 Mar 2025 5:50:59pm
+    Author:  Chris Bienvenu
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <vector>
+
+class DelayEffectProcessor{
+    
+    public:
+    DelayEffectProcessor(int delaySamples = 48000, float feedback = 0.5f)
+        : delaySamples(delaySamples), feedback(feedback)
+    {
+        buffer.resize(delaySamples, 0.0f);
+    }
+
+    void setFeedback(float newFeedback)
+    {
+        feedback = newFeedback;
+    }
+
+    float processSample(float x)
+    {
+        float delayedSample = buffer[readIndex];
+        float output = x + delayedSample * feedback;
+        
+        buffer[writeIndex] = x + delayedSample * feedback;
+        
+        readIndex = (readIndex + 1) % delaySamples;
+        writeIndex = (writeIndex + 1) % delaySamples;
+        
+        return output;
+    }
+
+private:
+    std::vector<float> buffer;
+    int delaySamples;
+    float feedback;
+    int readIndex = 0;
+    int writeIndex = 0;
+};
+
